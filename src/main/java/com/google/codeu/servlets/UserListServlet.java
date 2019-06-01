c100cb96f5d2db10c2b45903a95ee5a712c2535c
张嘉文 <zhangjiawen@zhangjiawendeMacBook-Air.local>
2019-06-01 19:44:45 -0400

    resolve merge conflict for UserListServlet

---
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -0,0 +1,42 @@
+package com.google.codeu.servlets;
+
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
+import java.io.IOException;
+
+import java.util.Set;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Handles fetching all users for the community page.
+ */
+@WebServlet("/user-list")
+public class UserListServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+ /**
+   * Responds with a JSON representation of all users. 
+   * Example JSON: ["user@test.com", "user2@test.com", "user3@example.com"]
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("application/json");
+    Set<String> users = datastore.getUsers();
+    Gson gson = new Gson();
+    String json = gson.toJson(users);
+    response.getOutputStream().println(json);
+    /**let other programs (Front-End) to read directly into variables */
+  }
+}
+
+
