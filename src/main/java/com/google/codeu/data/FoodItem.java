b61ea044deb01a3efc29b069ceebca176dbf059a
张嘉文 <zhangjiawen@zhangjiawendeMacBook-Air.local>
2019-06-23 23:07:34 -0400

    Add FoodItem as an Entity in Datastore

---
+++ src/main/java/com/google/codeu/data/FoodItem.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.codeu.data;
+
+
+/** A single message posted by a user. */
+public class FoodItem {
+
+  private String name;
+  public String amount;
+  public double co2peryear;
+
+  public FoodItem(String name, String amount, String country, double co2peryear) {
+    this.name = name;
+    this.amount = amount;
+    this.co2peryear = co2peryear;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAmount() {
+    return amount;
+  }
+
+
+  public long getCO2() {
+    return co2peryear;
+  }
+}
