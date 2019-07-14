32128a3aa86af88a8069989cec90e6c276f47b57
Darrell Branch <openfusegames@gmail.com>
2019-07-14 15:38:54 -0400

    Changed functionality to accept Post requests for JSON chart data First helper lastSevenDays created, but imcomplete

--- src/main/java/com/google/codeu/servlets/ChartsDataServlet.java
+++ src/main/java/com/google/codeu/servlets/ChartsDataServlet.java
@@ -1,11 +1,12 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,14 +14,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 
 @WebServlet("/chartsdata/*")
 public class ChartsDataServlet extends HttpServlet{
 	
-	private JsonArray userMealArray;
+	private DatastoreService datastore;
 
 	/*
 	 * Class that models the JSON for our dummy data.
@@ -28,105 +33,97 @@ public class ChartsDataServlet extends HttpServlet{
 	 * For now, it is assigned a random value
 	 * */
 	private static class UserMeal {
-		double carbonFootprint;
-		LocalDate date;
+		private UUID id;
+		private List<String> foods;
+		private List<Double> amounts;
+		private Date date;
 
-		private UserMeal(double carbonFootprint, LocalDate date) {
-			this.carbonFootprint = carbonFootprint;
+		private UserMeal(UUID id, List<String> foods, List<Double> amounts, Date date) {
+			this.id = id;
+			this.foods = foods;
+			this.amounts = amounts;
 			this.date = date;
 		}
 	}
 	
 	/*
 	 * Generates Dummy data to be sent in GET requests
-	 * Creates 21 user meals. 7 days * 3 Meals per day.
 	 * TODO: Connect this servlet with FoodItems and UserMeals in the Datastore.
 	 * */
 	@Override
 	public void init() {
-		this.userMealArray = new JsonArray();
-		Gson gson = new Gson();
-		for (int i = 0; i < 7; i++) {
-			LocalDate mealDate = LocalDate.now().minusDays(i);
-			for(int j = 0; j < 3; j++) {
-				//Carbon Footprint will be a random number between 1 and 50
-				double carbonFootprint = Math.random() * 49 + 1;
-				UserMeal userMeal = new UserMeal(carbonFootprint, mealDate);
-				this.userMealArray.add(gson.toJsonTree(userMeal));
-			}
-		}
+		this.datastore = DatastoreServiceFactory.getDatastoreService();
 	}
 
 	/*
-	 * Sends a JSON response after a GET request
-	 * Parses the wild-card route, and calls respective helper method to get data
+	 * Sends a JSON response after a POST request
+	 * Calls respective helper method to get data based on param analysisType
 	 * for that route.
-	 * Example JSON Response: [{carbonFootprint, date},{carbonFootprint, date}]
 	 * */
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String analysisType = request.getParameter("analysisType");
+		
+		if(analysisType.isEmpty()) {
+			response.sendError(400, "Invalid analysis type");
+		}
+		else if(analysisType.equals("lastSevenDays")) {
+			this.getLastSevenDays(request, response);
+		}
+		else {
+			response.sendError(400, "Invalid analysis type");
+		}
+	}
+	
+	/*
+	 * Returns JSON response for user meals from the last seven days
+	 * */
+	private void getLastSevenDays(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("application/json");
-		String requestUrl = request.getRequestURI();
+		String user = request.getParameter("user");
+		//Get the time seven days ago
+		LocalDate date = LocalDate.now().minusDays(7);
+		//Convert it to old style Date object
+		Date sevenDaysAgo = (Date) java.sql.Date.valueOf(date);
 		
-		if(requestUrl.endsWith("all")) {
-			response.getOutputStream().println(this.listMealsForUser());
-		}
-		if(requestUrl.endsWith("week")) {
-			response.getOutputStream().println(this.lastSevenDays());
-		}
-	}
-	
-	/*
-	 * Returns the string representation of the userMeals JSON array
-	 */
-	private String listMealsForUser() {
-		return this.userMealArray.toString();
-	}
-	
-	/*
-	 * Calculates the average carbon footprint  of each day of the last week
-	 * Returns the string representation of the JSON array.
-	 * Example output: {"2019-06-30" : 20.5675, "2019-06-32" : 13.765}
-	 * 
-	 * */
-	private String lastSevenDays() {
-		LocalDate date = LocalDate.now();
-		HashMap<LocalDate, ArrayList<Double>> carbonEntriesByDay = new HashMap<>();
-		HashMap<LocalDate, Double> averageCarbonPerDay = new HashMap<>();
+		FilterPredicate dateFilter = new Query.FilterPredicate("date", Query.FilterOperator.GREATER_THAN, sevenDaysAgo);
+		FilterPredicate userFilter = new Query.FilterPredicate("user", Query.FilterOperator.EQUAL, user);
+		
+		Query query = new Query("UserMeal")
+							.setFilter(new Query.CompositeFilter(Query.CompositeFilterOperator.AND, Arrays.asList(dateFilter, userFilter)));
+							
+		PreparedQuery results = datastore.prepare(query);
+		List<UserMeal> meals = this.processMessageQuery(results);
 		Gson gson = new Gson();
-		//Organize meals by Date -> [Meal1, Meal2, Meal,3]
-		for(int i = 0; i < this.userMealArray.size(); i++) {
-			UserMeal meal = gson.fromJson(this.userMealArray.get(i), UserMeal.class);
-			//Only include meals from within the last week
-			if(meal.date.isBefore(date.minusDays(7))) {
-				break;
-			}		
-			carbonEntriesByDay.putIfAbsent(meal.date, new ArrayList<Double>());
-			carbonEntriesByDay.get(meal.date).add(meal.carbonFootprint);
-		}
-		//Store the average: Date -> Average Carbon Footprint
-		for(LocalDate day: carbonEntriesByDay.keySet()) {
-			ArrayList<Double> values = carbonEntriesByDay.get(day);
-			double average = this.average(values);
-			averageCarbonPerDay.put(day, average);
-		}
-		
-		return gson.toJson(averageCarbonPerDay).toString();
+		String JsonRespone = gson.toJson(meals);
+		response.getOutputStream().println(JsonRespone);
 	}
 	
 	/*
-	 * Calculates the average of a list of double values
-	 * Returns sum if empty, the average otherwise
-	 * 
-	 * */
-	private Double average(List<Double> values) {
-		double sum = 0.0;
-		if(!values.isEmpty()) {
-			for(double number: values) {
-				sum += number;
+	 * Iterates through query results and returns a list of messages
+	 *
+	 * @return a list of messages.
+	 */
+	private List<UserMeal> processMessageQuery(PreparedQuery queryResults) {
+		List<UserMeal> meals = new ArrayList<>();
+
+		for (Entity entity : queryResults.asIterable()) {
+			try {
+				String idString = entity.getKey().getName();
+				UUID id = UUID.fromString(idString);
+				List<String> foods = (List<String>) entity.getProperty("foods");
+				List<Double> amounts = (List<Double>) entity.getProperty("amounts");
+				Date date = (Date) entity.getProperty("date");
+
+				UserMeal meal = new UserMeal(id, foods, amounts, date);
+				meals.add(meal);
+			} catch (Exception e) {
+				System.err.println("Error reading message.");
+				System.err.println(entity.toString());
+				e.printStackTrace();
 			}
-			return sum / values.size();
 		}
-		return sum;
+
+		return meals;
 	}
 }
